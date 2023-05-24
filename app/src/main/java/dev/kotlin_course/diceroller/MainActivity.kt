6fdb7daa35d5b8da327e4598bb654c3894c3a645
eduardo <eduardo.carneiro2907@gmail.com>
2023-05-24 18:50:55 -0300

    Add dice roll logic

--- app/src/main/java/dev/kotlin_course/diceroller/MainActivity.kt
+++ app/src/main/java/dev/kotlin_course/diceroller/MainActivity.kt
@@ -3,16 +3,29 @@ package dev.kotlin_course.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
+        val resultTextView: TextView = findViewById(R.id.textView)
+
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice rolled", Toast.LENGTH_SHORT)
-            toast.show()
+            resultTextView.text = rollDice()
         }
     }
+}
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
+fun rollDice(): String {
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    return diceRoll.toString()
 }
