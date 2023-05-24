113b8c9f7bf7db9d93ca07bf96f4bb2983d581bf
eduardo <eduardo.carneiro2907@gmail.com>
2023-05-24 20:07:52 -0300

    Replace text number with dice images

--- app/src/main/java/dev/kotlin_course/diceroller/MainActivity.kt
+++ app/src/main/java/dev/kotlin_course/diceroller/MainActivity.kt
@@ -1,30 +1,43 @@
 package dev.kotlin_course.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
-        val resultTextView: TextView = findViewById(R.id.textView)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener { resultTextView.text = rollDice() }
+
+    rollButton.setOnClickListener { rollDice() }
+
+    rollDice()
+  }
+
+  private fun rollDice() {
+    val diceImage: ImageView = findViewById(R.id.imageView)
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    val drawableResource = when (diceRoll) {
+      1 -> R.drawable.dice_1
+      2 -> R.drawable.dice_2
+      3 -> R.drawable.dice_3
+      4 -> R.drawable.dice_4
+      5 -> R.drawable.dice_5
+      else -> R.drawable.dice_6
     }
+
+    diceImage.setImageResource(drawableResource)
+    diceImage.contentDescription = diceRoll.toString()
+  }
 }
 
 class Dice(private val numSides: Int) {
 
-    fun roll(): Int {
-        return (1..numSides).random()
-    }
-}
-
-fun rollDice(): String {
-    val dice = Dice(6)
-    val diceRoll = dice.roll()
-    return diceRoll.toString()
+  fun roll(): Int {
+    return (1..numSides).random()
+  }
 }
